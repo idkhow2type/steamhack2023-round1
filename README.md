27e005889f74a18e44c2164c34aba4833007c492
idkhow2type <lecaominhhn@gmail.com>
2023-07-08 10:44:48 +0700

    finish setup, ejs template

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-```console
-npm i
-npm run dev
-```
+# Game plan
+- idk
+- you know how we have to write documentation?
+- like this?
+- yeah, i dont like that
+- so were making the computer do it
+- using ai obviously
+- and i dont have money
+- so were using this free chatgpt library thing
+- openai pls dont sue me
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
 import express from 'express'
-import fs from 'fs'
 import cookie from 'cookie-parser';
+import ask from 'gpt4free-ts'
 
 const app: express.Application = express();
-
-app.engine('html', (path, options, callback) => {
-    fs.readFile(path, (err, content) => {
-        if (err) return callback(err);
-        const rendered = content.toString().replace(/{{(\w+)}}/g, (_, p1) => {
-            return options[p1];
-        });
-        return callback(null, rendered);
-    });
-});
 
 app.use(express.static('public'));
 app.use(cookie());
 app.use(express.json());
 app.use(express.text());
 app.set('views', './views');
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 // routes start here
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.get('*', (req, res) => {
     res.status(404).send('404 not found');
 });
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Listening at http://localhost:${port}`);
 });
